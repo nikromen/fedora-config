95febfc0af979eccfd925ad056a44e9d1ba77bc0
Jiri Kyjovsky <j1.kyjovsky@gmail.com>
2021-12-30 15:22:24 +0100

    Added missing read

--- warnings.sh
+++ warnings.sh
@@ -4,5 +4,8 @@ read
 echo  -e "Make sure you have the access rights to your github, gitlab and pagure repos!"
 read
 
+echo -e "Are you sure? Enable fingerprints (gh, gl, pagure) and keys in ssh config should be named id_rsa_*"
+read
+
 echo -e "Check backup - [school stuff, keepass file, doc dir]!\e[0m"
 read
